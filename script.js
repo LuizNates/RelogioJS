014532ad1e30e69c910238384c3a3f48dc734de5
Luiz Gustavo Nates Caldato <luizgunates@gmail.com>
2024-07-04 16:21:27 -0300

    Versão Única e final

---
+++ script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var hora = 20
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        img.src = './imagens/manha.jpg'
+        document.body.style.background = '#069BF0'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = './imagens/tarde.jpg'
+        document.body.style.background = '#E37509'
+    } else {
+        img.src = './imagens/noite.jpg'
+        document.body.style.background = '#000F51'
+    }
+}
+
